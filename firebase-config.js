99a5b2b285dc726ffcaae8ee2154ffbc670b255a
Botyyyy <107973574+DeveloperPrabit@users.noreply.github.com>
2024-10-27 18:28:26 +0545

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,16 @@
-// Your web app's Firebase configuration
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDSqgOdoAoOjrOMIaj_pUuPPKSNCBYlhnQ",
-    authDomain: "backendapp-e7687.firebaseapp.com",
-    projectId: "backendapp-e7687",
-    storageBucket: "backendapp-e7687.appspot.com",
-    messagingSenderId: "816345679495",
-    appId: "1:816345679495:web:028109be772a9238419a22",
-    measurementId: "G-V06R8ZM8LN"
+  apiKey: "AIzaSyDSqgOdoAoOjrOMIaj_pUuPPKSNCBYlhnQ",
+  authDomain: "backendapp-e7687.firebaseapp.com",
+  projectId: "backendapp-e7687",
+  storageBucket: "backendapp-e7687.appspot.com",
+  messagingSenderId: "816345679495",
+  appId: "1:816345679495:web:028109be772a9238419a22",
+  measurementId: "G-V06R8ZM8LN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;;
+export default app;
